a0870526a0efb34401eee92509f4ab6c7de00e52
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-06-30 17:25:25 +0300

    Update Utils.java

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -5,16 +5,15 @@ import java.util.List;
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
         for (String s: args) {
-            if ( s == null)
-                return false;
             if (!isPositiveNumber(s))
                 return false;
         }
         return true;
-
     }
 
     public static boolean isPositiveNumber(String str) {
+        if (str == null)
+            return false;
         try {
             if(Double.parseDouble(str) > 0)
                 return true;
