7190d45237c949936c5558780ccae988aeaa233a
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-06-30 16:45:14 +0300

    Update StringUtils.java

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,7 +3,13 @@ package com.epam.utils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 public class StringUtils {
-    public static boolean isPositiveNumber(String str) {
-        return NumberUtils.isCreatable(str) && NumberUtils.toDouble(str) > 0;
+       public static boolean isPositiveNumber(String str) {
+        try {
+            if(Double.parseDouble(str) > 0)
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return false;
     }
 }
